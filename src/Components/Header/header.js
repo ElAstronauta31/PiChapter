6512528baf2440f71bf59a7b9a5bfc21fce50bb6
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-31 08:11:23 -0400

    header structure

---
+++ src/Components/Header/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div>
+            <div class="leftHeader">
+                <img></img>
+                <div>
+                    Pi Chpater | Phi Iota Alpha Fraternity, Inc.
+                </div>
+            </div>
+            <div class="rightHeader">
+                <ul>
+                    <li>Join</li>
+                    <li>About Us</li>
+                    <li>Brothers</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;

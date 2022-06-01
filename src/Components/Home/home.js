e4c937949b1bbbfb7e6116db400aded97d743203
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-06-01 11:16:36 -0400

    added stateless component to files

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Body from './body/body';
+import Highlight from './highlight/highlight';
+import Photos from './photos/photos';
 
 const Home = () => {
     return (
         <div>
-            
+            <Photos />
+            <Body />
+            <Highlight />
             
         </div>
     );

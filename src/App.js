1740ba36255ef8d19ebac36cac518ae209cefb6b
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-06-08 11:16:32 -0400

    Added BrowserRouter, switch and Route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import Header from "./Components/Header/header";
 import Home from "./Components/Home/home";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 
 function App() {
   return (
+
     <div className="App">
       <Header/>
       <Home />
